54f668cc289b0f30427b689ef86425de038d5af5
olmoser <o@omoser.com>
2014-08-28 16:34:56 +0200

    refactoring

--- api/viotualize-service-smartobject/src/main/java/com/viotualize/core/handlers/query/SmartObjectRepositoryQueries.java
+++ api/viotualize-service-smartobject/src/main/java/com/viotualize/core/handlers/query/SmartObjectRepositoryQueries.java
@@ -1,7 +1,12 @@
 package com.viotualize.core.handlers.query;
 
+import com.viotualize.core.repositories.SmartObjectRepository;
+import com.viotualize.core.repositories.domain.SmartObject;
+import org.springframework.stereotype.Component;
+
 /**
  * @author omoser
  */
-public interface SmartObjectRepositoryQueries {
+@Component
+public class SmartObjectRepositoryQueries extends HystrixProtectedRepositoryQueries<SmartObject, SmartObjectRepository> {
 }
