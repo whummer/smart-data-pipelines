74744cb1b07d195002f2d9e8c5785a309c12fe8f
olmoser <o@omoser.com>
2014-09-01 18:49:20 +0200

    renaming and moving EurekaPortAdvisor to eureka/ package

--- api/viotualize-common-core/src/main/java/com/viotualize/core/eureka/EurekaPortAdvisor.java
+++ api/viotualize-common-core/src/main/java/com/viotualize/core/eureka/EurekaPortAdvisor.java
@@ -1,4 +1,4 @@
-package com.viotualize.core.boot;
+package com.viotualize.core.eureka;
 
 import com.viotualize.core.logging.Markers;
 import org.slf4j.Logger;
@@ -10,9 +10,9 @@ import org.springframework.context.ApplicationListener;
  * @author omoser
  */
 
-public class ServicePortAdvisor implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+public class EurekaPortAdvisor implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
-    static final Logger log = LoggerFactory.getLogger(ServicePortAdvisor.class);
+    static final Logger log = LoggerFactory.getLogger(EurekaPortAdvisor.class);
 
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
