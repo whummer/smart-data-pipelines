25950b004e6274d6af3214cf2c05321d20cb787a
olmoser <o@omoser.com>
2014-09-04 14:28:29 +0200

    fixing wrong JAR value for Dockerfile

--- api/viotualize-common-service-base/src/main/resources/run.sh
+++ api/viotualize-common-service-base/src/main/resources/run.sh
@@ -4,4 +4,4 @@
 docker build -t riots/${project.artifactId} .
 
 # link to eureka and run the container. adapt port and container name.
-docker run -i -t -p ${riots.port}:${riots.port} --link eureka:eureka --name ${project.artifactId} riots/${project.artifactId}
+docker run --rm -i -t -p ${riots.port}:${riots.port} --link eureka:eureka --name ${project.artifactId} riots/${project.artifactId}
