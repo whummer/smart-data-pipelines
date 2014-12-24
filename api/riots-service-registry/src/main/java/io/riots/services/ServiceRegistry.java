0112d99d63c40eebcf3736556c0cf31fa480c6a8
Oliver Moser <o@omoser.com>
2014-12-24 13:39:00 +0100

    adding service registry for embedded Eureka setup

---
+++ api/riots-service-registry/src/main/java/io/riots/services/ServiceRegistry.java
@@ -0,0 +1,21 @@
+package io.riots.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by omoser on 22/12/14.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class ServiceRegistry {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistry.class, args);
+    }
+}
