c694cfa8ea7bc1b6c7dee8a3b88272643e978581
Oliver Moser <o@omoser.com>
2015-01-05 20:44:14 +0100

    moving urlrewrite rules from WEB-INF to root folder

--- api/riots-webapp/src/main/java/io/riots/webapp/WebappServiceStarter.java
+++ api/riots-webapp/src/main/java/io/riots/webapp/WebappServiceStarter.java
@@ -1,5 +1,6 @@
 package io.riots.webapp;
 
+import io.riots.core.filters.RiotsUrlRewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +9,11 @@ import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
-import io.riots.core.filters.RiotsUrlRewriteFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
-//@ComponentScan(basePackages = "io.riots")
 @ImportResource(value = {"classpath:/spring-noauth.xml"})
 public class WebappServiceStarter extends SpringBootServletInitializer {
 
@@ -23,15 +21,14 @@ public class WebappServiceStarter extends SpringBootServletInitializer {
 	 * URL rewriting filter. See file urlrewrite.xml for configuration.
 	 * @return
 	 */
-	/*@Bean
+	@Bean
 	public FilterRegistrationBean urlRewriteFilter(){
 	    FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
 	    RiotsUrlRewriteFilter filter = new RiotsUrlRewriteFilter();
 	    filterRegBean.setFilter(filter);
-	    filterRegBean.addInitParameter("confPath", "/WEB-INF/urlrewrite.xml");
+	    filterRegBean.addInitParameter("confPath", "urlrewrite.xml");
 	    return filterRegBean;
 	}
-*/
 	public static void main(String[] args) {
 		if(System.getProperty("RIOTS_LOG_DIR") == null) {
 			System.setProperty("RIOTS_LOG_DIR", "log");
