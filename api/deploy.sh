76433ac082ad894f313c971a35c416b009c5497c
Waldemar Hummer <whummer@hummer.io>
2015-01-09 19:39:02 +0100

    fix #60 - easy fix for now

--- api/deploy.sh
+++ api/deploy.sh
@@ -6,6 +6,7 @@ chmod 600 $BASEDIR/../infra/ssh-keys/google_compute_engine
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $BASEDIR/../infra/ssh-keys/google_compute_engine ubuntu@146.148.126.222 <<'ENDSSH'
 FIG_FILE=/root/riots/infra/fig.yml
+(cd /root/riots/ && git pull)
 sudo -u root fig -f $FIG_FILE pull --allow-insecure-ssl 
 sudo -u root fig -f $FIG_FILE up -d
 ENDSSH
