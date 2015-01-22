402f8be575c9264550463af184692442a0890699
Oliver Moser <o@omoser.com>
2015-01-22 13:49:31 +0100

    fixing issue with ES data dir (test fixture was not removed)

--- api/riots-service-catalog/src/test/java/io/riots/api/services/catalog/CatalogServiceTest.java
+++ api/riots-service-catalog/src/test/java/io/riots/api/services/catalog/CatalogServiceTest.java
@@ -1,18 +1,18 @@
 package io.riots.api.services.catalog;
 
-import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.when;
+import com.jayway.restassured.filter.log.RequestLoggingFilter;
+import com.jayway.restassured.filter.log.ResponseLoggingFilter;
+import com.jayway.restassured.http.ContentType;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.Application;
 import io.riots.api.services.catalog.model.ThingTypeElastic;
 import io.riots.boot.starters.CatalogServiceTestStarter;
 import io.riots.core.clients.ServiceClientFactory;
 import io.riots.core.repositories.ThingTypeRepository;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-
+import org.apache.cxf.helpers.FileUtils;
 import org.apache.http.HttpStatus;
+import org.elasticsearch.client.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +20,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.jayway.restassured.filter.log.RequestLoggingFilter;
-import com.jayway.restassured.filter.log.ResponseLoggingFilter;
-import com.jayway.restassured.http.ContentType;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.shared.Application;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.mockito.Mockito.when;
 
 /**
  * @author omoser
@@ -49,7 +58,7 @@ public class CatalogServiceTest extends AbstractTestNGSpringContextTests {
 	String endpoint;
 
 	@Autowired
-	EmbeddedWebApplicationContext server;
+	EmbeddedWebApplicationContext context;
 
 	@Autowired
 	ThingTypeRepository thingTypeRepository;
@@ -60,17 +69,39 @@ public class CatalogServiceTest extends AbstractTestNGSpringContextTests {
 	@Autowired
 	DiscoveryClient discoveryClient;
 
-	@BeforeClass
-	public void elasticFixture() {
+	@Autowired
+	Client nodeClient;
 
+	@Value("${basedir}/target/data")
+	String dataDirectory;
+
+	@BeforeClass
+	public void elasticFixture() throws InterruptedException {
 		// please note that we have to set the port here since it is not known in CatalogServiceTestStarter
 		InstanceInfo.Builder builder = InstanceInfo.Builder.newBuilder();
 		Application catalogService = new Application("catalog-service");
-		catalogService.addInstance(builder.setHostName("localhost").setIPAddr("127.0.0.1").
-				setPort(port).setAppName("catalog-service").build());
+		catalogService.addInstance(builder
+				.setHostName("localhost")
+				.setIPAddr("127.0.0.1")
+				.setPort(port)
+				.setAppName("catalog-service")
+				.build()
+		);
+
 		when(discoveryClient.getApplication("catalog-service")).thenReturn(catalogService);
 
 		// setup a sample thing type
+		ThingType ultraSonicSensor = createSampleThingType();
+		thingTypeRepository.save(new ThingTypeElastic(ultraSonicSensor));
+	}
+
+	@AfterClass
+	public void cleanUpDataDir() {
+		// remove ES data dir in mvn clean was not called
+		FileUtils.removeDir(new File(dataDirectory));
+	}
+
+	private ThingType createSampleThingType() {
 		ThingType ultraSonicSensor = new ThingType("HC-SR04");
 		ultraSonicSensor.withImageData(Arrays.asList(
 				new ImageData()
@@ -95,8 +126,7 @@ public class CatalogServiceTest extends AbstractTestNGSpringContextTests {
 		propDist.setActuatable(false).setSensable(true);
 		propDist.setValueDomain(new ValueDomainContinuous<>(0.0, 200.0));
 		ultraSonicSensor.getProperties().add(propDist);
-
-		thingTypeRepository.save(new ThingTypeElastic(ultraSonicSensor));
+		return ultraSonicSensor;
 	}
 
 	@Test
@@ -122,9 +152,13 @@ public class CatalogServiceTest extends AbstractTestNGSpringContextTests {
 	public void checkSingleItemUsingServiceClient() {
 //		TODO: FIXME does not work in my case (number of thing types 
 //		keeps increasing when running test multiple times)
-//		CatalogService client = clientFactory.getCatalogServiceClient();
-//		List<? extends ThingType> thingTypes = client.listThingTypes(null, 0, 100);
-//		assertThat("contains single ThingType HC-SR04", thingTypes.size(), equalTo(1));
+		CatalogService client = clientFactory.getCatalogServiceClient();
+		List<? extends ThingType> thingTypes = client.listThingTypes(null, 0, 100);
+		assertThat("contains single ThingType HC-SR04", thingTypes.size(), equalTo(1));
+		ThingType expected = createSampleThingType();
+		ThingType actual = thingTypes.get(0);
+		expected.setId(actual.getId());
+		//assertThat("ThingType from ES is equal to expected thingtype", actual, samePropertyValuesAs(expected));
 	}
 
 }
