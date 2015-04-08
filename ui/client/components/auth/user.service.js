74f6092978714bf1dc5352242fc7abbddd7971f8
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-04-08 14:17:07 +0200

    minor

--- ui/client/components/auth/user.service.js
+++ ui/client/components/auth/user.service.js
@@ -2,7 +2,7 @@
 
 angular.module('rioxApp')
   .factory('User', function ($resource) {
-    return $resource('/api/users/:id/:controller', {
+    return $resource('/api/v1/users/:id/:controller', {
       id: '@_id'
     },
     {
