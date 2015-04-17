902a009cea0daf8044dad5f4ef6a10af39d4d121
Oliver Moser <o@omoser.com>
2015-04-17 15:06:08 +0200

    fixing "_" link creation in preinstall.js; removing index.html and adding it to .gitignore

--- nodejs/bin/preinstall.js
+++ nodejs/bin/preinstall.js
@@ -7,8 +7,6 @@ var _ = require('lodash');
 var service_directories = ['streams-service', 'users-service'];
 var web_ui_directory = 'web-ui';
 
-console.log("Preinstalling node modules");
-
 function preinstall_dir(base, dir) {
 	var preinstallDirectory = resolve(__dirname, join(base, dir));
 	console.log('Preinstalling node modules in ', preinstallDirectory);
@@ -16,10 +14,19 @@ function preinstall_dir(base, dir) {
 		return;
 	}
 
-	// install folder
-	cp.spawn('npm', ['i'], {env: process.env, cwd: preinstallDirectory, stdio: 'inherit'})
+	cp.exec('npm i', {env: process.env, cwd: preinstallDirectory}, function(error, stdout, stderr) {
+		console.log('Finished node_modules install in dir: ', preinstallDirectory);
+		var nodeModulesDir = join(preinstallDirectory, 'node_modules');
+		if (!fs.existsSync(join(nodeModulesDir, "/_"))) {
+			console.log("Creating _ link in node_modules directory: ", nodeModulesDir);
+			cp.exec('ln -sf ../../lib _', {env: process.env, cwd: nodeModulesDir}, function(error, stdout, stderr) {
+				if (error) {
+					console.log("Cannot create _ link in " + nodeModulesDir + ": ", stderr);
+				}
+			});
+		}
+	});
 }
-
 
 // preinstall web-ui
 preinstall_dir('..', web_ui_directory);
