6ddea45cef0a3a6ec8d8f85db15dcfd2d148800d
Oliver Moser <o@omoser.com>
2015-04-20 14:36:23 +0200

    removing lodash dependency in preinstall.js

--- nodejs/bin/preinstall.js
+++ nodejs/bin/preinstall.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var resolve = require('path').resolve;
 var join = require('path').join;
 var cp = require('child_process');
-var _ = require('lodash');
+//var _ = require('lodash');
 
 var service_directories = ['streams-service', 'users-service'];
 var web_ui_directory = 'web-ui';
@@ -31,7 +31,9 @@ function preinstall_dir(base, dir) {
 // preinstall web-ui
 preinstall_dir('..', web_ui_directory);
 
+preinstall_dir('..', 'services');
+
 // preinstall service directories
-_.forEach(service_directories, function (service_dir) {
+service_directories.forEach(function (service_dir) {
 	preinstall_dir('../services', service_dir);
 });
