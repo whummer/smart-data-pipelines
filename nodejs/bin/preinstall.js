80681d583ae11f7a5fa50fe017d9d6f750f13c35
Oliver Moser <o@omoser.com>
2015-04-27 13:18:11 +0200

    polishing preinstall.js. Now uses `linklocal` for installing (actually linking) local dependencies/node modules

--- nodejs/bin/preinstall.js
+++ nodejs/bin/preinstall.js
@@ -3,9 +3,22 @@ var resolve = require('path').resolve;
 var join = require('path').join;
 var cp = require('child_process');
 
-var service_directories = ['streams-service', 'users-service'];
+var service_directories = ['streams-service', 'users-service', 'riox-services-base'];
 var services_test_directory = 'services/test';
 var web_ui_directory = 'web-ui';
+
+const USE_LINK_LOCAL = true;
+const PRUNE_NODE_DIRS = false;
+
+if (USE_LINK_LOCAL) {
+	console.log("********************************************************************************");
+	console.log("********************************************************************************");
+	console.log("                  MAKE SURE YOU HAVE 'linklocal' installed");
+	console.log("                 (install it with 'npm install -g linklocal)  ");
+	console.log("********************************************************************************");
+	console.log("********************************************************************************");
+	console.log("");
+}
 
 function preinstall_dir(base, dir) {
 	var preinstallDirectory = resolve(__dirname, join(base, dir));
@@ -14,18 +27,61 @@ function preinstall_dir(base, dir) {
 		return;
 	}
 
-	cp.exec('npm i', {env: process.env, cwd: preinstallDirectory}, function (error, stdout, stderr) {
+	if (PRUNE_NODE_DIRS) {
+		prune_node_directories(preinstallDirectory, function() {
+			install_node_modules(preinstallDirectory)
+		});
+	} else {
+		install_node_modules(preinstallDirectory);
+	}
+}
+
+function prune_node_directories(preinstallDirectory, callback) {
+	console.log("Pruning node_modules directory in ", preinstallDirectory);
+	cp.exec('npm prune', {env: process.env, cwd: preinstallDirectory}, function (error, stdout, stderr) {
+		if (error) {
+			console.log("Cannot prune node modules in directory '" + preinstallDirectory + "': ", error);
+			return;
+		}
+
+		callback();
+	});
+}
+
+function install_node_modules(preinstallDirectory) {
+	cp.exec('npm install', {env: process.env, cwd: preinstallDirectory}, function (error, stdout, stderr) {
+		if (error) {
+			console.log("Cannot install node modules in directory '" + preinstallDirectory + "': ", error);
+			return;
+		}
+
 		console.log('Finished node_modules install in dir: ', preinstallDirectory);
-		var nodeModulesDir = join(preinstallDirectory, 'node_modules');
-		if (!fs.existsSync(join(nodeModulesDir, "/_"))) {
-			console.log("Creating _ link in node_modules directory: ", nodeModulesDir);
-			cp.exec('ln -sf ../../lib _', {env: process.env, cwd: nodeModulesDir}, function (error, stdout, stderr) {
-				if (error) {
-					console.log("Cannot create _ link in " + nodeModulesDir + ": ", stderr);
-				}
-			});
+		if (USE_LINK_LOCAL) {
+			link_lib_with_linklocal(preinstallDirectory);
+		} else {
+			link_lib_with_lodash(preinstallDirectory);
 		}
 	});
+}
+
+function link_lib_with_linklocal(preinstallDirectory) {
+	cp.exec('linklocal -r', {env: process.env, cwd: preinstallDirectory}, function(error, stdout, stderr) {
+		if (error) {
+			console.log("Cannot linklocal modules in '" + preinstallDirectory + "': ", error);
+		}
+	});
+}
+
+function link_lib_with_lodash(preinstallDirectory) {
+	var nodeModulesDir = join(preinstallDirectory, 'node_modules');
+	if (!fs.existsSync(join(nodeModulesDir, "/_"))) {
+		console.log("Creating _ link in node_modules directory: ", nodeModulesDir);
+		cp.exec('ln -sf ../../lib _', {env: process.env, cwd: nodeModulesDir}, function (error, stdout, stderr) {
+			if (error) {
+				console.log("Cannot create _ link in " + nodeModulesDir + ": ", stderr);
+			}
+		});
+	}
 }
 
 // preinstall web-ui
