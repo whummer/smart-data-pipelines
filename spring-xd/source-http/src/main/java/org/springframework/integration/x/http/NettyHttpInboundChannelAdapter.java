5904fcd1f42a665346db8d54f43eb235acc3132e
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-05-16 17:34:00 +0200

    reduce logging output

--- spring-xd/source-http/src/main/java/org/springframework/integration/x/http/NettyHttpInboundChannelAdapter.java
+++ spring-xd/source-http/src/main/java/org/springframework/integration/x/http/NettyHttpInboundChannelAdapter.java
@@ -277,7 +277,7 @@ public class NettyHttpInboundChannelAdapter extends MessageProducerSupport {
 		if (bootstrap != null) {
 			bootstrap.shutdown();
 		}
-		logger.info("Removing channel adapter from map: " + port + ":" + path);
+		logger.debug("Removing channel adapter from map: " + port + ":" + path);
 		Map<String, Object> pathToProducer = getPathsToProducers();
 		pathToProducer.remove(path);
 		System.gc();
@@ -431,7 +431,6 @@ public class NettyHttpInboundChannelAdapter extends MessageProducerSupport {
 
 	@SuppressWarnings("unchecked")
 	private void initModuleState() {
-		System.out.println("--> initModuleState");
 		synchronized (this.getClass()) {
 			/* bit hacky, store module state in system properties */
 			if (!System.getProperties().containsKey(KEY_MODULE_STATE)) {
@@ -454,8 +453,7 @@ public class NettyHttpInboundChannelAdapter extends MessageProducerSupport {
 			SenderEntry e = new SenderEntry();
 			e.senderMethod = getSenderMethod(this);
 			e.senderEntity = this;
-			System.out.println("--> Putting channel adapter to map: " + port + ":" + path + " - " + e);
-			logger.info("Putting channel adapter to map: " + port + ":" + path + " - " + e);
+			logger.debug("Putting channel adapter to map: " + port + ":" + path + " - " + e);
 			pathsToProducers.put(path, e);
 		}
 	}
