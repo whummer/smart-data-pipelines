de60cfcc78089fa1b173e849a04e5a8cd41bea62
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-07-22 10:22:49 +0200

    fix services config variable in Web UI (riox now running e2e on my local machine)

--- nodejs/web-ui/lib/app/config.js
+++ nodejs/web-ui/lib/app/config.js
@@ -12,28 +12,10 @@ if(typeof window != "undefined") {
 	_host = window.location.host;
 }
 
-var appConfig = window.appConfig = servicesConfig = {
+var appConfig = window.appConfig = {
 	appRootPath: "/app",
 	bowerRootPath: "/bower_components",
 	services: {
-//		core: { url: "http://" + _host + ":8080/api/v1" },
-//		streams: { url: "http://" + _host + ":8085/api/v1/streams" },
-//		organizations: { url: "http://" + _host + ":8084/api/v1/organizations" },
-//		billing: { url: "http://" + _host + ":8080/api/v1/billing" },
-//		users: { url: "http://" + _host + ":8084/api/v1/users" },
-//		streamsources: { url: "http://" + _host + ":8085/api/v1/streams/sources" },
-//		streamsinks: { url: "http://" + _host + ":8085/api/v1/streams/sinks" },
-//		streamprocessors: { url: "http://" + _host + ":8085/api/v1/streams/processors" },
-//	    statistics: { url: "http://" + _host + ":8085/api/v1/statistics" },
-//	    ratings: { url: "http://" + _host + ":8085/api/v1/ratings" },
-//		access: { url: "http://" + _host + ":8085/api/v1/access" },
-//		files: { url: "http://" + _host + ":8087/api/v1/files" },
-//		notifications: { url: "http://" + _host + ":8084/api/v1/notifications" },
-//	    analytics: { url: "http://" + _host + ":8086/api/v1/analytics" },
-//	    consents: { url: "http://" + _host + ":8085/api/v1/consents" },
-//		certificates: { url: "http://" + _host + ":8084/api/v1/certificates" },
-//	    statisticsWebsocket: { url: "ws://" + _host + ":8085/api/v1/statistics/live" },
-
 		streams: { url: "//" + _host + "/api/v1/streams" },
 		organizations: { url: "//" + _host + "/api/v1/organizations" },
 		billing: { url: "//" + _host + "/api/v1/billing" },
@@ -61,6 +43,9 @@ var appConfig = window.appConfig = servicesConfig = {
 		'admin'		// admin
 	]
 };
+
+/* set servicesConfig variable */
+window.servicesConfig = appConfig.services;
 
 if(typeof module != "undefined") {
 	module.exports = appConfig;
