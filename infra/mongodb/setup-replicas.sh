0e6be11939be2d2b5a024c8e0fa5e282e7f7db20
Oliver Moser <o@omoser.com>
2015-07-26 19:31:14 +0200

    adding missing changes in Makefile and replica setup script

--- infra/mongodb/setup-replicas.sh
+++ infra/mongodb/setup-replicas.sh
@@ -6,12 +6,19 @@
 # * collect the IP addresses of the mongo PODs that were created by the mongo replication controller
 # * add the list of IP addresses to the replica set config
 #
+MONGO_PRIMARY=mongo-1
+DOMAIN=development.svc.cluster.local
+MONGO_DNS_NAME="${MONGO_PRIMARY}.${DOMAIN}"
+MONGO_SERVICE_IP=$(dig +short ${MONGO_DNS_NAME})
+REPLICA_MAX_ID=3
+#echo -e "rs.initiate()" | mongo $MONGO_SERVICE_IP # todo this is a workaround for poor omoser since something is fubar 
 
-MONGO_SERVICE_IP=$(dig +short mongo.dev.svc.cluster.local)
-echo -e "rs.initiate()" | mongo $MONGO_SERVICE_IP # todo this is a workaround for poor omoser since something is fubar 
-
-MONGO_IPS=`docker inspect $(docker ps | grep mongo | grep POD | awk '{print $1}') | grep IPA | grep -vi secondary | cut -d ':' -f2 | cut -d '"' -f2`
-for IP in $MONGO_IPS; do
-	echo "Adding $IP to mongo replica set";
-	echo -e "rs.add('$IP:27017')" | mongo $MONGO_SERVICE_IP
+MONGO_CONFIG="rs.initiate();"
+for REPLICA_ID in `seq 2 $REPLICA_MAX_ID`; do
+	MONGO_CONFIG="$MONGO_CONFIG; rs.add('mongo-${REPLICA_ID}.${DOMAIN}:27017');";
 done
+
+MONGO_CONFIG="$MONGO_CONFIG; var newconf = rs.conf(); newconf.members[0].host = 'mongo-1.development.svc.cluster.local:27017'; rs.reconfig(newconf);";
+echo -e "$MONGO_CONFIG" | mongo $MONGO_SERVICE_IP
+
+#echo -e "rs.add('${MONGO_SECONDARY_1}:27017'); rs.add('${MONGO_SECONDARY_2}:27017');var newconf = rs.conf(); newconf.members[0].host = 'mongo-1.development.svc.cluster.local:27017'; rs.reconfig(newconf);" | mongo $MONGO_SERVICE_IP
