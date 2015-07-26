a0f6b4db8385e99bcf0b84009c66d070930e5e95
Oliver Moser <o@omoser.com>
2015-07-26 19:49:00 +0200

    adding check for IP4 boot2docker address instead of hardcoded default 192...

--- infra/bin/addRouteForBoot2Docker.sh
+++ infra/bin/addRouteForBoot2Docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo route -n add 10.0.0.0/16 192.168.59.103
+BOOT2DOCKER_ETH1_IP=$(boot2docker ssh ifconfig eth1  | grep "inet addr:"  | awk '{print $2}' | cut -d':' -f2)
+sudo route -n add 10.0.0.0/16 $BOOT2DOCKER_ETH1_IP
