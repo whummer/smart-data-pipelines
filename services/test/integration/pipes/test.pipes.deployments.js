963dabc99b3aa0e08ee026421432aa0bca10cf32
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-09-02 12:01:20 +0200

    fix k8s descriptors (metadata label)

--- services/test/integration/pipes/test.pipes.deployments.js
+++ services/test/integration/pipes/test.pipes.deployments.js
@@ -78,7 +78,7 @@ describe('pipes.deployment', function() {
 			app.pipes.server.stop();
 
 		// recorder.after(done);
-		//resources.server.close();
+		resources.server.close();
 		done();
 	});
 
@@ -130,95 +130,95 @@ describe('pipes.deployment', function() {
 			});
 	});
 
-//	it ('deploys a valid pipeline and its succeeds', function(done) {
-//		this.timeout(75000);
-//
-//		var content = JSON.parse(fs.readFileSync(path.join(__dirname, './resources') + '/ma-vienna-pipe.js'));
-//		if(isLocalNonKubernetesRun()) {
-//			content = JSON.parse(JSON.stringify(content).replace(
-//					/integration-tests\.test\.svc\.cluster\.local:6789/g,
-//					'172.17.42.1:6789'));
-//		}
-//
-//		// 1. Add a sample pipe and get the ID
-//		test.user1.post(app.pipes.url).send(content).end(function(err, res) {
-//			if (err) {
-//				log.error('Unexpected error: ', err)
-//				should.fail();
-//			}
-//			expect(res.body.id).to.exist;
-//
-//			// 2. Define the POST /deployments payload
-//			var payload = {}
-//			payload[PIPE_ID] = res.body.id,
-//
-//			test.user1
-//				.post(app.pipes.deployments.url)
-//				.send(payload)
-//				.end(function(err, res) {
-//					if (err) {
-//						log.error('Unexpected error: ', err);
-//						should.fail();
-//					}
-//
-//					res.status.should.equal(201);
-//					expect(res.body).to.exist;
-//					let content = res.body;
-//					log.debug('Body: ', JSON.stringify(content));
-//
-//					expect(content.id).to.exist;
-//
-//					var locationHeader = res.get("location");
-//
-//					test.user1
-//						.get(locationHeader)
-//						.send()
-//						.end(function(err, res) {
-//
-//							log.debug("Response: ", res.body);
-//							expect(res.body[PIPE_ID]).to.equal(payload[PIPE_ID])
-//
-//							// check deployment status for each element
-//							Promise.each(res.body[STATUS], function(element) {
-//								expect(element.status).to.equal("deployed");
-//								expect(element.uuid).to.exist;
-//							})
-//							.then( function() {
-//								// query elasticsearch and ensure stuff from the pipeline made it there
-//								let url = "http://elasticsearch."+ process.env.RIOX_ENV +".svc.cluster.local:9200/smartcity/waitingtimes/_search?q=shortName:MBA3"
-//								superagent.get(url).send().end(function(err, res) {
-//									log.debug("Elasticsearch response: ", res.body);
-//									expect(res.body.hits).to.exist;
-//									expect(res.body.hits.total).to.be.above(0);
-//									Promise.each(res.body.hits.hits, function(element) {
-//										// log.debug("Element: ", JSON.stringify(element));
-//										expect(element._source.waitingTime).to.be.above(-1);
-//										expect(element._source.location).to.exist;
-//										expect(element._source.shortName).to.exist;
-//										expect(element._source.IsOpen).to.exist;
-//										expect(element._source.timestamp).to.exist;
-//										expect(element._source.NAME).to.exist;
-//									})
-//									.then( function() {
-//										test.user1
-//											.delete(locationHeader)
-//											.send()
-//											.end(function(err, res) {
-//												log.debug("Response: ", res.body);
-//												if (err) {
-//													log.error("Error: ", err);
-//												}
-//												res.status.should.equal(200);
-//												// .then( function() { done() } );
-//												// TODO ensure streams are really gone
-//												done();
-//											});
-//									});
-//							});
-//					});
-//				});
-//			});
-//		});
-//	});
+	it ('deploys a valid pipeline and its succeeds', function(done) {
+		this.timeout(75000);
+
+		var content = JSON.parse(fs.readFileSync(path.join(__dirname, './resources') + '/ma-vienna-pipe.js'));
+		if(isLocalNonKubernetesRun()) {
+			content = JSON.parse(JSON.stringify(content).replace(
+					/integration-tests\.test\.svc\.cluster\.local:6789/g,
+					'172.17.42.1:6789'));
+		}
+
+		// 1. Add a sample pipe and get the ID
+		test.user1.post(app.pipes.url).send(content).end(function(err, res) {
+			if (err) {
+				log.error('Unexpected error: ', err)
+				should.fail();
+			}
+			expect(res.body.id).to.exist;
+
+			// 2. Define the POST /deployments payload
+			var payload = {}
+			payload[PIPE_ID] = res.body.id,
+
+			test.user1
+				.post(app.pipes.deployments.url)
+				.send(payload)
+				.end(function(err, res) {
+					if (err) {
+						log.error('Unexpected error: ', err);
+						should.fail();
+					}
+
+					res.status.should.equal(201);
+					expect(res.body).to.exist;
+					let content = res.body;
+					log.debug('Body: ', JSON.stringify(content));
+
+					expect(content.id).to.exist;
+
+					var locationHeader = res.get("location");
+
+					test.user1
+						.get(locationHeader)
+						.send()
+						.end(function(err, res) {
+
+							log.debug("Response: ", res.body);
+							expect(res.body[PIPE_ID]).to.equal(payload[PIPE_ID])
+
+							// check deployment status for each element
+							Promise.each(res.body[STATUS], function(element) {
+								expect(element.status).to.equal("deployed");
+								expect(element.uuid).to.exist;
+							})
+							.then( function() {
+								// query elasticsearch and ensure stuff from the pipeline made it there
+								let url = "http://elasticsearch."+ process.env.RIOX_ENV +".svc.cluster.local:9200/smartcity/waitingtimes/_search?q=shortName:MBA3"
+								superagent.get(url).send().end(function(err, res) {
+									log.debug("Elasticsearch response: ", res.body);
+									expect(res.body.hits).to.exist;
+									expect(res.body.hits.total).to.be.above(0);
+									Promise.each(res.body.hits.hits, function(element) {
+										// log.debug("Element: ", JSON.stringify(element));
+										expect(element._source.waitingTime).to.be.above(-1);
+										expect(element._source.location).to.exist;
+										expect(element._source.shortName).to.exist;
+										expect(element._source.IsOpen).to.exist;
+										expect(element._source.timestamp).to.exist;
+										expect(element._source.NAME).to.exist;
+									})
+									.then( function() {
+										test.user1
+											.delete(locationHeader)
+											.send()
+											.end(function(err, res) {
+												log.debug("Response: ", res.body);
+												if (err) {
+													log.error("Error: ", err);
+												}
+												res.status.should.equal(200);
+												// .then( function() { done() } );
+												// TODO ensure streams are really gone
+												done();
+											});
+									});
+							});
+					});
+				});
+			});
+		});
+	});
 
 });
